a0b249f32ed7ca67a967109c54866e83942b4b41
RaymondAya-ay <raymond.p.ayaay@gmail.com>
2020-12-06 20:57:14 +0800

    just added some stuff, nothing worth mentioning yet

--- src/GUI.java
+++ src/GUI.java
@@ -1,27 +1,60 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+    private static JLabel userLabel;                private static JTextField userText;
+    private static JLabel passwordLabel;    private static JPasswordField passwordText;
+    private static JButton button;                  private static JLabel successLabel;
 
     public static void main(String[] args){
 
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
 
-
-        frame.setSize(300, 300);
+        frame.setSize(350, 200);
         frame.setTitle("Bruh");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         frame.add(panel);
         panel.setLayout(null);
 
-        JLabel label = new JLabel("User");
-        label.setBounds(10,20,80,25);
-        panel.add(label);
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10,20,80,25);
+        panel.add(userLabel);
 
-        JTextField userText = new JTextField();
+        userText = new JTextField(20);
+        userText.setBounds(100,20, 165, 25);
+        panel.add(userText);
+
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10,50,80,25);
+        panel.add(passwordLabel);
+
+        passwordText = new JPasswordField();
+        passwordText.setBounds(100,50,165,25);
+        panel.add(passwordText);
+
+        button = new JButton("Login");
+        button.setBounds(10,80,80,25);
+        button.addActionListener(new GUI());
+        panel.add(button);
+
+        successLabel = new JLabel("");
+        successLabel.setBounds(10,110,300,25);
+        panel.add(successLabel);
+
 
         frame.setVisible(true);
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        char[] password = passwordText.getPassword();
+
+        System.out.println(user + ", " + new String(password));
     }
 }
